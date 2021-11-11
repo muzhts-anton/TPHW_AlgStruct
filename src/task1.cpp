cb4de7369395a6c1e22178413fbd438ac21f063e
Мужецкий Антон <tonich2001@gmail.com>
2021-11-12 02:03:31 +0300

    fix: refactoring

--- src/task1.cpp
+++ src/task1.cpp
@@ -2,17 +2,11 @@
 
 int main()
 {
-    std::cout << "Yo, give me N and K:\n> ";
-    int num = 0;
-    int byteplace = 0;
+    std::cout << "Yo, give me N and K:\n";
+    int num = 0, byteplace = 0;
     std::cin >> num >> byteplace;
-    if (byteplace < 0)
-        return 1;
 
-    int byte = 0;
-    num >>= byteplace;
-    byte = num & 1;
-    std::cout << byte << std::endl;
+    std::cout << ((num >> byteplace) & 1) << std::endl;
 
     return 0;
 }
